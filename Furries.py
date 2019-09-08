ee14ae20250768f2a4a3a7653318130f0f5b4019
RixHuskypaw <42594866+RixHuskypaw@users.noreply.github.com>
2019-09-08 19:45:13 -0400

    Add files via upload

---
+++ Furries.py
@@ -0,0 +1,40 @@
+mixed_word = 'eiswneo'
+#first letter
+first = "F"
+second_to_last_letters = mixed_word[1::]
+#first equation
+first_letter = first + second_to_last_letters
+#second letter
+second = "u"
+#second equation
+third_to_last_letters = mixed_word[2::]
+second_letter = first + second + third_to_last_letters
+#third letter
+third = "r"
+#third equation
+fourth_to_last_letters = mixed_word[3::]
+third_letter = first + second + third + fourth_to_last_letters
+#fourth letter
+fourth = "r"
+#fourth equation
+fifth_to_last_letters = mixed_word[4::]
+fourth_letter = first + second + third + fourth + fifth_to_last_letters
+#fifth letter
+fifth = "i"
+#fifth equation
+sixth_to_last_letter = mixed_word[5::]
+fifth_letter = first + second + third + fourth + fifth + sixth_to_last_letter
+#sixth letter
+sixth = "e"
+#sixth letter equation
+seventh_to_last_letter = mixed_word[6::]
+sixth_letter = first + second + third + fourth + fifth + sixth + seventh_to_last_letter
+#seventh letter
+seventh = "s"
+#seventh equation
+eighth_letter = mixed_word[7::]
+seventh_letter = first + second + third + fourth + fifth + sixth + seventh + eighth_letter
+
+print(seventh_letter)
+
+# Word = "Furries"
